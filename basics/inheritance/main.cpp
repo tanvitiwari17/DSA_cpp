a9e6b351ca9fb42a37a06391173c9bf808701cac
tanvitiwari17 <tanvitiwari1700@gmail.com>
2020-09-29 18:06:40 +0530

    29/09

---
+++ basics/inheritance/main.cpp
@@ -0,0 +1,23 @@
+/*
+
+~ Inheritance the process of inheriting properties and behaviors of existing class into a new class.
+~ Existing class is also known as 'old class' or 'parent class' or 'base class'.
+~ New class is called 'child class' or 'derived class'.
+
+Syntax:
+class base_class
+{
+};
+class derived_class : visibility_mode base_class
+{
+};
+
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
