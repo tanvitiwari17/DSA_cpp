f23e0473d6bf3330d90e9f31a02779d1b04ccddb
tanvitiwari17 <tanvitiwari1700@gmail.com>
2020-10-02 14:31:38 +0530

    02/10

--- basics/file_handling/main.cpp
+++ basics/file_handling/main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
+/*
+File Handling
 
+-Data Persistence: life of data
+-Stream: flow of data
+        from program to file - output stream
+        from file to program - input stream
+            - istream -istream_withassign -(cin)
+                      -ifstream
+     ios-|            -iostream-|
+                                 - fstream
+            - ostream -iostream-|
+                      -ofstream
+                      -ostream_withassign -(cout)
+
+to write into file - ofstream obj
+to open file - open(".."), if file is not present in the hard disc, open creates a new file
+*/
+
+#include<fstream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    ofstream fout;
+    fout.open("myfile.dat"); // open is a member fn of ofstream, to open file
+    fout<<"hello";
+    fout.close();
     return 0;
 }
