e540b10eec93d75ad7b599fc2cba92f30b5872a9
tanvitiwari17 <tanvitiwari1700@gmail.com>
2020-10-09 16:40:04 +0530

    09/10

---
+++ basics/STL/main.cpp
@@ -0,0 +1,30 @@
+/*
+STL : Standard Template Library (powerful set of cpp template classes)
+At the core of the cpp STL are following three well-structured components:
+-Containers
+-Algorithms
+-Iterators
+
+Containers:
+    ~ used to manage collections of abjects of a certain kind.
+    ~ provides containers that are used to create data structures like arrays,linked list,trees,etc.
+    ~ these containers are generic, can hold elements of any data types
+
+eg:-Vectors: used for creating dynamic arrays of char,int, float and other types
+
+Algorithms:
+    ~ act on containers.
+    ~ provide means by which we perform initialization, sorting,searching, and transforming of contents of containers.
+    ~ contains built in functions that perform complex algorithms on data structures.
+    ~ provides abstraction
+
+eg:- reverse(),sort(),binary_search()
+
+Iterators:
+    ~ used to step through the elements of collections of object.These collections may be
+      containers or subsets of containers.
+    ~ used to point to the containers.
+    ~ acts as a bridge between containers and algorithms.
+
+eg: sort() algorithm have two parameters, starting iterator and ending iterator.
+*/
