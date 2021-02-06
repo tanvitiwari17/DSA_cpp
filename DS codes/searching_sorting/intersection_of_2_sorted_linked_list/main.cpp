22e242d5ab02de33a354b92fd3edc7a4a386c5ed
Tanvi Tiwari <51092839+tanvitiwari17@users.noreply.github.com>
2021-02-06 23:24:43 +0530

    06/02

---
+++ DS codes/searching_sorting/intersection_of_2_sorted_linked_list/main.cpp
@@ -0,0 +1,27 @@
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* temp1=head1;
+    Node* temp2=head2;
+    Node* head=NULL;
+    Node* temp=NULL;
+    while(temp1!=NULL and temp2!=NULL)
+    {
+        if(temp1->data == temp2->data)
+        {
+            Node* new_node = new Node(temp1->data);
+            if(head)
+            temp->next = new_node;
+            else
+            head= new_node;
+            temp=new_node;
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        else if(temp1->data<temp2->data)
+        temp1=temp1->next;
+        else
+        temp2=temp2->next;
+    }
+    return head;
+    }
