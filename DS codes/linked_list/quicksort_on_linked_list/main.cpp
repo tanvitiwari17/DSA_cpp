fb5adea65598ea2a6d36d0c8f4b2b95af8bbca48
Tanvi Tiwari <51092839+tanvitiwari17@users.noreply.github.com>
2021-02-11 22:38:32 +0530

    11/02

---
+++ DS codes/linked_list/quicksort_on_linked_list/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/* a node of the singly linked list */
+struct node
+{
+    int data;
+    struct node *next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* A utility function to insert a node at the beginning of linked list */
+void push(struct node** head_ref, int new_data)
+{
+    /* allocate node */
+    struct node* new_node = new node(new_data);
+
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+
+/* A utility function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+void quickSort(struct node **headRef);
+int main()
+{
+	int t,n,x;
+	cin>>t;
+	while(t--){
+
+
+        cin>>n;n=n-1;
+		cin>>x;
+        node *temp,*head = new node(x);
+        temp=head;
+        while(n--)
+        {
+			cin>>x;
+          temp->next=new node(x);
+          temp=temp->next;
+			}
+
+    quickSort(&head);
+
+    printList(head);
+ while(head!= NULL){
+     temp=head;
+     head=head->next;
+     free(temp);
+
+ }
+	}
+    return 0;
+}// } Driver Code Ends
+
+
+
+/* a node of the singly linked list
+struct node
+{
+    int data;
+    struct node *next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}; */
+
+node* partition(node* head,node* tail){
+    node* prev=head;
+    node* curr=head->next;
+    node* pivot = head;
+    while(curr!=tail->next){
+        if(curr->data<pivot->data)
+        {
+            swap(prev->next->data,curr->data);
+            prev= prev->next;
+        }
+        curr=curr->next;
+    }
+        swap(pivot->data,prev->data);
+        return prev;
+}
+void quicksortrec(node *head,node* tail)
+{
+    if(head==tail or tail==NULL or head==NULL)
+    return;
+    node *pivot=partition(head,tail);
+    quicksortrec(head,pivot);
+    quicksortrec(pivot->next,tail);
+}
+//you have to complete this function
+void quickSort(struct node **headRef) {
+
+    node*tail = *headRef;
+    while(tail->next!=NULL)
+    tail = tail->next;
+    quicksortrec(*headRef,tail);
+}
