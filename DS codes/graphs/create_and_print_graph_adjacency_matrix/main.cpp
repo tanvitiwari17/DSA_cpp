688db9558cb101a03f47625153f3a34a5004e662
Tanvi Tiwari <51092839+tanvitiwari17@users.noreply.github.com>
2021-02-25 22:40:30 +0530

    25/02

---
+++ DS codes/graphs/create_and_print_graph_adjacency_matrix/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int v,e;
+    cin>>v>>e;
+    int g[v+1][v+1];
+    for(int i=1;i<=v;i++){
+            for(int j=1;j<=v;j++)
+            g[i][j]=0;
+    }
+    for(int i=0;i<e;i++){
+        int x,y;
+        cin>>x>>y;
+        g[x][y]=1;
+        g[y][x]=1;
+    }
+
+    for(int i = 1;i<=v;i++){
+        cout<<i<<"-->";
+        for(int j=1;j<=v;j++){
+            cout<<g[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
