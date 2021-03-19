3acdbd648bf357c5148977e91f4099b540205945
Tanvi Tiwari <51092839+tanvitiwari17@users.noreply.github.com>
2021-03-19 22:56:46 +0530

    19/03

---
+++ DS codes/strings/anagrams/main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+vector<vector<string> > Anagrams(vector<string>& string_list) ;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+vector<vector<string> > Anagrams(vector<string>& string_list)
+{
+    // Your Code Here
+    vector<vector<string>>res;
+    unordered_map<string,vector<string>>s;
+    for(int i=0;i<string_list.size();i++)
+    {
+        string x = string_list[i];
+        sort(x.begin(),x.end());
+        s[x].push_back(string_list[i]);
+    }
+    for(auto x:s){
+        res.push_back(x.second);
+    }
+    return res;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i];
+        vector<vector<string> > result = Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
